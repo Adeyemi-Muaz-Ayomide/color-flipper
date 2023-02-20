016486e8723afcce0cdf2887b994eeaf48aaa6eb
iAmMuadh <adeyemimuaz1@gmail.com>
2023-02-20 16:18:38 +0100

    added an event listener and a function

--- scripts.js
+++ scripts.js
@@ -2,6 +2,10 @@ let clickEl = document.getElementById('click-el');
 let backColor = document.getElementById('bgColor');
 let arrayColors = ['red','yellow','blue', 'green'];
 
+clickEl.addEventListener('click' , function() {
+    backColor.textContent = 'Background Color: ' + modifiedRandColor();
+
+});
 
 
 
