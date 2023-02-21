84213d6bdbd0e267bb49098a293a1c6f5dae8980
iAmMuadh <adeyemimuaz1@gmail.com>
2023-02-21 01:23:59 +0100

    changed the getElementById to querySelector

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 let clickEl = document.getElementById('click-el');
-let backColor = document.getElementById('bgColor');
+let backColor = document.querySelector('.color');
 let arrayColors = ['red','yellow','blue', 'green'];
 
 clickEl.addEventListener('click' , function() {
-    backColor.textContent = 'Background Color: ' + modifiedRandColor();
+    backColor.textContent =  modifiedRandColor();
 
 });
 
